c233272699656d7ce847e8a75380e5d154ad6c4c
thelonehegelian <thelonehegelian@gmail.com>
2020-12-23 00:42:16 +0000

    Separate Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Rating from './Rating'
 
 function Product ({product}) {
 	return(
@@ -16,23 +16,7 @@ function Product ({product}) {
                   <a href={`/product/${product._id}`}>
                     <h2>{product.name}</h2>
                   </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
+                  	<Rating rating = {product.rating} numReviews = {product.numReviews}/>
                   <div className="price">${product.price}</div>
                 </div>
               </div>
